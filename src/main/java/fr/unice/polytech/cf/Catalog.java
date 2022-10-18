03bed3cf0d9fc6332f22d2dbe1644a30f23dae48
Vinh <narutovinh@hotmail.fr>
2022-10-18 15:06:24 +0200

    Avancement catalog #1

---
+++ src/main/java/fr/unice/polytech/cf/Catalog.java
@@ -0,0 +1,26 @@
+package fr.unice.polytech.cf;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Catalog {
+    private List<Cookie> cookies;
+
+    public Catalog() {
+        this.cookies = new ArrayList<>();
+        this.cookies.add(new Cookie("Chocolate"));
+    }
+
+    public List<Cookie> getCookies() {
+        return this.cookies;
+    }
+
+    public boolean getCookie(String name) {
+        for (Cookie cookie : this.cookies) {
+            if (cookie.getName().equals(name)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
