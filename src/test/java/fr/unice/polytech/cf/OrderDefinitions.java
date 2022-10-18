4ceef0ff4a43e00ea74698ea5d9785f62b9e3719
Tom Bevan <bevantom@protonmail.com>
2022-10-18 16:00:06 +0200

    Correction erreurs cucumber #1

--- src/test/java/fr/unice/polytech/cf/OrderDefinitions.java
+++ src/test/java/fr/unice/polytech/cf/OrderDefinitions.java
@@ -4,22 +4,26 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class OrderDefinitions {
     Cart cart ;
     Catalog catalog;
 
-    @Given("the catalog contains the cookie {string}")
+    /*@Given("the catalog contains the cookie {String}")
     public void the_catalog_contains_cookie(String cookie) {
         if (catalog.hasCookie(cookie)) {
             cart.addCookie(new Cookie( cookie));
         }
 
+    }*/
+
+    @Given("the cart contains {int} cookies")
+    public void theCartContainsCookies(int number) {
+        cart = new Cart();
+        for (int i = 0; i<number; i++)
+            cart.addCookie(new Cookie("chocolat"));
     }
 
-    @When("the client add {int} cookie\\(s) to the cart")
+    @When("the client add {int} {word} to the cart")
     public void the_client_add_cookie_s_to_the_cart(Integer amount, String cookie) {
         for (int i = 0; i<amount; i++)
             cart.addCookie(new Cookie( cookie));
@@ -36,5 +40,6 @@ public class OrderDefinitions {
             System.out.println("the catalog doesn't contain the cookie");
         }
     }
+
 
 }
