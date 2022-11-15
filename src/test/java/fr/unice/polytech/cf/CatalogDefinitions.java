ee09465de5042a37b232940245cc576d8de5a272
bernigaud-lecoq <benjamin.bernigaud@gmail.com>
2022-11-15 15:35:54 +0100

    #13 adding test to catalog
    ajout de test sur catalog

--- src/test/java/fr/unice/polytech/cf/CatalogDefinitions.java
+++ src/test/java/fr/unice/polytech/cf/CatalogDefinitions.java
@@ -1,16 +1,40 @@
 package fr.unice.polytech.cf;
 
 import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 
 public class CatalogDefinitions {
     Catalog catalog;
-    boolean possible;
 
+    @Given("we create a catalog")
+    public void we_create_a_catalog(){
+        catalog=new Catalog();
+
+    }
     @And("the catalog contains the cookie {word}")
     public void the_catalog_contains_cookie(String cookie) {
         if (!catalog.hasCookie(cookie))
             catalog.addCookie(new Cookie(cookie));
-        possible = true;
+
         assert(catalog.hasCookie(cookie));
+    }
+    @Then("the catalog contains the cookies {word}")
+    public void the_catalog_contains_cookies(String cookies){
+        if (!catalog.hasCookie(cookies))
+            catalog.addCookie(new Cookie(cookies));
+
+        assert(catalog.hasCookie(cookies));
+    }
+    @Then("the catalog doesn't contain the cookies {word}")
+    public void the_catalog_doesnt_contain(String cookies){
+        if (!catalog.hasCookie(cookies))
+            catalog.addCookie(new Cookie(cookies));
+
+        assert(!catalog.hasCookie(cookies));
+    }
+    @And("we add a cookie {word} to the catalog")
+    public void we_add_a_cookie_to_the_catalog(String cookie){
+        catalog.addCookie(new Cookie(cookie));
     }
 }
