c171a7ee1034af2ce12555f78cd86d226886758e
quentinBourdeau <quentin.bourdeau@etu.univ-cotedazur.fr>
2022-11-29 16:14:43 +0100

    added StoreRepository #22

---
+++ src/main/java/fr/unice/polytech/cf/repositories/StoreRepository.java
@@ -0,0 +1,9 @@
+package fr.unice.polytech.cf.repositories;
+
+import fr.unice.polytech.cf.entities.Order;
+import fr.unice.polytech.repositories.BasicRepositoryImpl;
+
+import java.util.UUID;
+
+public class StoreRepository extends BasicRepositoryImpl<Order, UUID> {
+}
