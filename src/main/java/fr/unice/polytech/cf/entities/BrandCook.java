dbc49e0681204310f36f0f85dce5651c7e6ff4d0
Vinh <narutovinh@hotmail.fr>
2022-12-15 16:21:53 +0100

    Brand correction #22

--- src/main/java/fr/unice/polytech/cf/entities/BrandCook.java
+++ src/main/java/fr/unice/polytech/cf/entities/BrandCook.java
@@ -1,23 +1,22 @@
 package fr.unice.polytech.cf.entities;
 
 import fr.unice.polytech.cf.components.CatalogHandler;
-import fr.unice.polytech.cf.entities.cookies.BasicCookie;
+import fr.unice.polytech.cf.entities.cookies.CookieRecipe;
 
 public class BrandCook {
     CatalogHandler catalog;
     BrandOwner brandOwner;
 
-    public BrandCook(BrandOwner b, CatalogHandler c){
+    public BrandCook(BrandOwner b, CatalogHandler c) {
         catalog = c;
         brandOwner = b;
     }
 
-    public boolean addCookie(BasicCookie basicCookie) {
-        if (brandOwner.acceptRecipe(basicCookie)) {
-            catalog.addCookie(basicCookie);
+    public boolean addCookie(CookieRecipe cookieRecipe) {
+        if (brandOwner.acceptRecipe(cookieRecipe)) {
+            catalog.addCookie(cookieRecipe);
             return true;
-        }
-        else {
+        } else {
             throw new RuntimeException("Too expensive");
         }
     }
--- src/main/java/fr/unice/polytech/cf/entities/BrandOwner.java
+++ src/main/java/fr/unice/polytech/cf/entities/BrandOwner.java
@@ -1,20 +1,20 @@
 package fr.unice.polytech.cf.entities;
 
 import fr.unice.polytech.cf.components.CatalogHandler;
-import fr.unice.polytech.cf.entities.cookies.BasicCookie;
+import fr.unice.polytech.cf.entities.cookies.CookieRecipe;
 
 public class BrandOwner {
     CatalogHandler catalog;
 
-    public BrandOwner(CatalogHandler c){
+    public BrandOwner(CatalogHandler c) {
         catalog = c;
     }
 
-    public void removeCookie(BasicCookie basicCookie) {
-        catalog.removeCookie(basicCookie);
+    public void removeCookie(CookieRecipe cookieRecipe) {
+        catalog.removeCookie(cookieRecipe);
     }
 
-    public boolean acceptRecipe(BasicCookie c){
+    public boolean acceptRecipe(CookieRecipe c) {
         return c.getPrice() < 3.5;
     }
 }
