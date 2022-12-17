6475817876b3cd482aa62a1bac39f8b978e1ad88
Vinh <narutovinh@hotmail.fr>
2022-12-17 17:48:37 +0100

    Suppression des dernières classes de Test non utilisées

--- src/test/java/fr/unice/polytech/cf/components/StoreManagementTest.java
+++
@@ -1,9 +0,0 @@
-package fr.unice.polytech.cf.components;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-public class StoreManagementTest {
-//    @Autowired
-}
