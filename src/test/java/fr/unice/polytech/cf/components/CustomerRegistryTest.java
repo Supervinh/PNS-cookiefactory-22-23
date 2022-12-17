36fc1a942f13fc86acd05c4dbd1d665096f12d7f
bernigaud-lecoq <benjamin.bernigaud@gmail.com>
2022-12-17 02:44:00 +0100

    test kitchen

--- src/test/java/fr/unice/polytech/cf/components/CustomerRegistryTest.java
+++ src/test/java/fr/unice/polytech/cf/components/CustomerRegistryTest.java
@@ -1,9 +1,63 @@
 package fr.unice.polytech.cf.components;
 
+import fr.unice.polytech.cf.entities.Customer;
+import fr.unice.polytech.cf.exceptions.AlreadyExistingCustomerException;
+import fr.unice.polytech.cf.interfaces.explorer.CustomerFinder;
+import fr.unice.polytech.cf.interfaces.modifier.CustomerRegistration;
+import fr.unice.polytech.cf.repositories.CustomerRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 public class CustomerRegistryTest {
-    //@Autowired
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private CustomerRegistration registry;
+
+    @Autowired
+    private CustomerFinder finder;
+
+    private String name = "John";
+    private String surname = "wick";
+    private String mail="johnwick@gmail.com";
+
+    @BeforeEach
+    void setUp() {
+        customerRepository.deleteAll();
+    }
+
+    @Test
+    public void unknownCustomer() {
+        assertFalse(finder.findByName(name).isPresent());
+    }
+
+    @Test
+    public void registerCustomer() throws Exception {
+        Customer returned = registry.register(name, surname,mail);
+        Optional<Customer> customer = finder.findByName(name);
+        assertTrue(customer.isPresent());
+        Customer john = customer.get();
+        assertEquals(john,returned);
+        assertEquals(john,finder.findById(returned.getId()).get());
+        assertEquals(name, john.getName());
+        assertEquals(surname, john.getSurname());
+    }
+
+    @Test
+    public void cannotRegisterTwice() throws Exception {
+        registry.register(name, surname,mail);
+        Assertions.assertThrows( AlreadyExistingCustomerException.class, () -> {
+            registry.register(name, surname,mail);
+        });
+    }
 }
--- src/test/java/fr/unice/polytech/cf/components/KitchenTest.java
+++ src/test/java/fr/unice/polytech/cf/components/KitchenTest.java
@@ -1,9 +1,74 @@
 package fr.unice.polytech.cf.components;
 
+import fr.unice.polytech.cf.entities.*;
+import fr.unice.polytech.cf.entities.cookies.BasicCookie;
+import fr.unice.polytech.cf.entities.ingredients.Cooking;
+import fr.unice.polytech.cf.entities.ingredients.Ingredient;
+import fr.unice.polytech.cf.entities.ingredients.IngredientEnum;
+import fr.unice.polytech.cf.entities.ingredients.Mix;
+import fr.unice.polytech.cf.interfaces.modifier.CustomerRegistration;
+import fr.unice.polytech.cf.interfaces.modifier.OrderProcessing;
+import fr.unice.polytech.cf.repositories.CustomerRepository;
+import fr.unice.polytech.cf.repositories.OrderRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 public class KitchenTest {
-    //@Autowired
+    @Autowired
+    CustomerRepository customerRepository;
+
+    @Autowired
+    OrderRepository orderRepository;
+
+    @Autowired
+    private CustomerRegistration registry;
+
+    @Autowired
+    private OrderProcessing processor;
+
+
+
+    private Set<Item> items;
+    private Customer john;
+    LocalTime time= LocalTime.of(8,30);
+    LocalTime time2= LocalTime.of(20,30);
+    private Store store;
+    LocalDateTime date=LocalDateTime.of(2022,12,17,22,0);
+    @BeforeEach
+    public void setUpContext() throws Exception {
+        customerRepository.deleteAll();
+        orderRepository.deleteAll();
+        store=new Store("store1",0.1,time,time2);
+        items = new HashSet<>();
+        items.add(new Item(new BasicCookie("chocolate", Cooking.CRUNCHY,
+                new Ingredient(store.getId(), IngredientEnum.DOUGH, "Chocolate", 3),
+                new Ingredient(store.getId(), IngredientEnum.FLAVOUR, "Cinnamon", 2.5),
+                Mix.MIXED, new ArrayList<>()), 3));
+        items.add(new Item(new BasicCookie("DARK_TEMPTATION", Cooking.CRUNCHY,
+                new Ingredient(store.getId(), IngredientEnum.DOUGH, "Chocolate", 3),
+                new Ingredient(store.getId(), IngredientEnum.FLAVOUR, "Cinnamon", 2.5),
+                Mix.MIXED, new ArrayList<>()), 1));
+        john = registry.register("john", "wick","johnwick@gmail.com");
+    }
+
+    @Test
+    void processCommand() throws Exception {
+        Order inProgress = new Order(john, items,store.getId(),date);
+        inProgress.setOrderState(OrderState.PAID);
+        processor.prepareOrder(inProgress);
+        assertEquals(inProgress.getOrderState(), OrderState.WORKING_ON_IT);
+        processor.finishOrder(inProgress);
+        assertEquals(OrderState.READY,inProgress.getOrderState() );
+    }
 }
