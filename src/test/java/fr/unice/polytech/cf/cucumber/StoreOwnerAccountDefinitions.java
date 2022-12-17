8bc95a5d7976c2419f83d9d713f75c01c35a0582
TomBevan <bevantom@protonmail.com>
2022-12-17 14:21:10 +0100

    Ajout des StoreIwnerAccoutDefinitions #22

--- src/test/java/fr/unice/polytech/cf/cucumber/StoreOwnerAccountDefinitions.java
+++ src/test/java/fr/unice/polytech/cf/cucumber/StoreOwnerAccountDefinitions.java
@@ -3,74 +3,81 @@ package fr.unice.polytech.cf.cucumber;
 import fr.unice.polytech.cf.entities.Store;
 import fr.unice.polytech.cf.entities.StoreOwnerAccount;
 import fr.unice.polytech.cf.entities.ingredients.Ingredient;
+import fr.unice.polytech.cf.interfaces.explorer.StoreFinder;
+import fr.unice.polytech.cf.interfaces.modifier.StoreModifier;
+import fr.unice.polytech.cf.repositories.StoreRepository;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.LocalTime;
 
 public class StoreOwnerAccountDefinitions {
-    /*StoreOwnerAccount storeOwnerAccount = new StoreOwnerAccount("Cookie factory Biot");
-    @Given("the store's opening time is {int}:{int}")
-    public void theStoreOpeningTimeIs(int hours, int minutes ) {
-        Store ownedStore = storeOwnerAccount.getOwnedStore();
-        ownedStore.setOpeningTime(LocalTime.of(hours, minutes));
+
+    @Autowired
+    private StoreRepository storeRepository;
+
+    @Autowired
+    private StoreModifier storeModifier;
+
+    @Autowired
+    private StoreFinder storeFinder;
+
+    @Before
+    public void settingUpContext() {
+        storeRepository.deleteAll();
     }
 
-    @When("I change the opening time to {int}:{int}")
-    public void iChangeTheOpeningTimeTo(int hours, int minutes) {
-        storeOwnerAccount.changeStoreOpeningTime(LocalTime.of(hours, minutes));
+    @Given("the store {word} opens at {int}:{int}")
+    public void theStoreOpeningTimeIs(String storeName, int hours, int minutes ) {
+        LocalTime openingTime = LocalTime.of(hours, minutes);
+        storeModifier.addStore(storeName, openingTime, openingTime.plusHours(8));
     }
 
-    @Then("the store's opening time should be {int}:{int}")
-    public void theStoreSOpeningTimeShouldBe(int hours, int minutes) {
-        Store ownedStore = storeOwnerAccount.getOwnedStore();
-        System.out.println(ownedStore.getOpeningTime());
-        assert( ownedStore.getOpeningTime().equals(LocalTime.of(hours,minutes)));
+    @When("I change {word}'s opening time to {int}:{int}")
+    public void iChangeTheOpeningTimeTo(String storeName, int hours, int minutes) {
+        storeModifier.changeStoreOpeningTime(storeFinder.findByName(storeName).get(), LocalTime.of(hours, minutes));
     }
 
-    @Given("the store's closing time is {int}:{int}")
-    public void theStoreClosingTimeIs(int hours, int minutes ) {
-        Store ownedStore = storeOwnerAccount.getOwnedStore();
-        ownedStore.setClosingTime(LocalTime.of(hours, minutes));
+    @Then("{word}'s opening time should be {int}:{int}")
+    public void theStoreSOpeningTimeShouldBe(String storeName, int hours, int minutes) {
+        assert(storeFinder.findByName(storeName).get().getOpeningTime().equals(LocalTime.of(hours,minutes)));
     }
 
-    @When("I change the closing time to {int}:{int}")
-    public void iChangeTheClosingTimeTo(int hours, int minutes) {
-        storeOwnerAccount.changeStoreClosingTime(LocalTime.of(hours, minutes));
+    @Given("the store {word} closes at {int}:{int}")
+    public void theStoreClosingTimeIs(String storeName, int hours, int minutes ) {
+        LocalTime closingTime = LocalTime.of(hours, minutes);
+        storeModifier.addStore(storeName, closingTime.minusHours(8), closingTime);
     }
 
-    @Then("the store's closing time should be {int}:{int}")
-    public void theStoreSClosingTimeShouldBe(int hours, int minutes) {
-        Store ownedStore = storeOwnerAccount.getOwnedStore();
-        System.out.println(ownedStore.getClosingTime());
-        assert( ownedStore.getClosingTime().equals(LocalTime.of(hours,minutes)));
+    @When("I change {word}'s closing time to {int}:{int}")
+    public void iChangeTheClosingTimeTo(String storeName, int hours, int minutes) {
+        storeModifier.changeStoreClosingTime(storeFinder.findByName(storeName).get(), LocalTime.of(hours, minutes));
     }
 
-    @Given("the store's taxes are {int}%")
-    public void theStoreSTaxesAre(int taxes) {
-        Store ownedStore = storeOwnerAccount.getOwnedStore();
-        ownedStore.setTaxes(taxes/100.0);
+    @Then("{word}'s closing time should be {int}:{int}")
+    public void theStoreSClosingTimeShouldBe(String storeName, int hours, int minutes) {
+        assert(storeFinder.findByName(storeName).get().getClosingTime().equals(LocalTime.of(hours,minutes)));
     }
 
-    @When("I change the taxes to {int}%")
-    public void iChangeTheTaxesTo(int taxes) {
-        storeOwnerAccount.changeStoreTaxes(taxes/100.0);
+    @Given("the store {word} with a tax rate at {int}%")
+    public void theStoreSTaxesAre(String storeName, int taxes) {
+        LocalTime openingTime = LocalTime.now();
+        storeModifier.addStore(storeName, openingTime, openingTime.plusHours(8));
+        storeModifier.changeStoreTaxes(storeFinder.findByName(storeName).get(), taxes/100.0);
     }
 
-    @Then("the store's taxes should be {int}%")
-    public void theStoreSTaxesShouldBe(int expectedTaxeRate) {
-        Store ownedStore = storeOwnerAccount.getOwnedStore();
-        assert( ownedStore.getTaxes() == expectedTaxeRate/100.0);
+    @When("I change {word}'s tax rate to {int}%")
+    public void iChangeTheTaxesTo(String storeName, int taxes) {
+        storeModifier.changeStoreTaxes(storeFinder.findByName(storeName).get(), taxes/100.0);
     }
 
-    @And("the ingredients' prices should be taxed by {int}%")
-    public void theIngredientsPricesShouldBeTaxedBy(int taxAppliedToIngredients) {
-        Store ownedStore = storeOwnerAccount.getOwnedStore();
-        for (Ingredient ingredient : ownedStore.getIngredients().keySet()) {
-            double expectedPrice = Math.floor(ingredient.getBasePrice() * (1 + taxAppliedToIngredients/100.0) * 100) / 100;
-            assert(ingredient.getPrice() == expectedPrice);
-        }
-    }*/
+    @Then("{word}'s tax rate should be {int}%")
+    public void theStoreSTaxesShouldBe(String storeName, int expectedTaxRate) {
+        assert( storeFinder.findByName(storeName).get().getTaxes() == expectedTaxRate/100.0);
+    }
+
 }
--- src/test/resources/features/store/StoreOwnerAccount.feature
+++ src/test/resources/features/store/StoreOwnerAccount.feature
@@ -1,39 +1,36 @@
 Feature: StoreOwnerAccount management
 
-#  Scenario: The store's opening hour is after the new opening hour
-#    Given the store's opening time is 13:00
-#    When I change the opening time to 12:00
-#    Then the store's opening time should be 12:00
-#
-#  Scenario: The store's opening hour is before the new opening hour
-#    Given the store's opening time is 15:00
-#    When I change the opening time to 16:00
-#    Then the store's opening time should be 16:00
-#
-#  Scenario: The store's closing hour is after the new closing hour
-#    Given the store's closing time is 19:00
-#    When I change the closing time to 18:00
-#    Then the store's closing time should be 18:00
-#
-#  Scenario: The store's closing hour is before the new closing hour
-#    Given the store's closing time is 17:00
-#    When I change the closing time to 19:00
-#    Then the store's closing time should be 19:00
-#
-#  Scenario: The store's taxes are lower than the new taxes
-#    Given the store's taxes are 0%
-#    When I change the taxes to 5%
-#    Then the store's taxes should be 5%
-#    And the ingredients' prices should be taxed by 5%
-#
-#  Scenario: The store's taxes are higher than the new taxes
-#    Given the store's taxes are 10%
-#    When I change the taxes to 15%
-#    Then the store's taxes should be 15%
-#    And the ingredients' prices should be taxed by 15%
-#
-#  Scenario: The store's taxes are equal to the new taxes
-#    Given the store's taxes are 10%
-#    When I change the taxes to 10%
-#    Then the store's taxes should be 10%
-#    And the ingredients' prices should be taxed by 10%
+  Scenario: The store's opening hour is after the new opening hour
+    Given the store myCucumberStore opens at 13:00
+    When I change myCucumberStore's opening time to 12:00
+    Then myCucumberStore's opening time should be 12:00
+
+  Scenario: The store's opening hour is before the new opening hour
+    Given the store myCucumberStore opens at 15:00
+    When I change myCucumberStore's opening time to 16:00
+    Then myCucumberStore's opening time should be 16:00
+
+  Scenario: The store's closing hour is after the new closing hour
+    Given the store myCucumberStore closes at 19:00
+    When I change myCucumberStore's closing time to 18:00
+    Then myCucumberStore's closing time should be 18:00
+
+  Scenario: The store's closing hour is before the new closing hour
+    Given the store myCucumberStore closes at 17:00
+    When I change myCucumberStore's closing time to 19:00
+    Then myCucumberStore's closing time should be 19:00
+
+  Scenario: The store's taxes are lower than the new taxes
+    Given the store myCucumberStore with a tax rate at 0%
+    When I change myCucumberStore's tax rate to 5%
+    Then myCucumberStore's tax rate should be 5%
+
+  Scenario: The store's taxes are higher than the new taxes
+    Given the store myCucumberStore with a tax rate at 10%
+    When I change myCucumberStore's tax rate to 15%
+    Then myCucumberStore's tax rate should be 15%
+
+  Scenario: The store's taxes are equal to the new taxes
+    Given the store myCucumberStore with a tax rate at 10%
+    When I change myCucumberStore's tax rate to 10%
+    Then myCucumberStore's tax rate should be 10%
